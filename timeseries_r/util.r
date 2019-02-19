07fa0be476b0f98a3766ba028608a5f5c16732d9
Chris Snow <chsnow123@gmail.com>
2019-02-19 14:07:38 +0000

    add patterns

--- timeseries_r/util.r
+++ timeseries_r/util.r
@@ -7,6 +7,12 @@ install_packages <- function(x, quietly=FALSE, warn.conflicts=TRUE){
   }
 }
 
+get_bond_yields_uk_10y_data_ts <- function(required_dataset) 
+{
+    data = get_bond_yields_uk_10y_data(required_dataset)
+    return(ts(data$Rate, start = c(1984, 03), frequency = 4))
+}
+
 get_bond_yields_uk_10y_data <- function(required_dataset) 
 {
     stopifnot(required_dataset %in% c('quarterly_csv.csv', 'abc'))
@@ -36,4 +42,23 @@ get_bond_yields_uk_10y_data <- function(required_dataset)
     }
     options(warn = oldw)
     return(data)
+}
+
+get_stedmondsbury_new_homes_ts <- function() 
+{
+    # https://data.gov.uk/dataset/6eb509cd-be2b-4ef5-882c-84d9146e5522/house-sales-and-prices
+    url = 'https://data.cambridgeshireinsight.org.uk/sites/default/files/ons-new-build-count-corrected_0.csv'
+
+    raw <- read.csv(url(url))
+    stedmundsbury = raw[ raw$Area == 'St Edmundsbury', 3:ncol(raw) ]
+    return(ts(t(stedmundsbury), start=c(1995, 4), frequency=4))
+}
+
+get_ibm_daily_close_change_ts <- function()
+{
+    options("getSymbols.warning4.0"=FALSE)  
+    options("getSymbols.yahoo.warning"=FALSE)
+    getSymbols("IBM",src="yahoo")
+    daily_change = diff(Cl(IBM))
+    return(daily_change)
 }
